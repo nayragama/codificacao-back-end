160a17a4a7dbf61a8b39c1118391745b198c5e40
Nayra Gama <nayrafgama263@gmail.com>
2026-09-21 19:40:27 -0300

    feat: carregar e validar variáveis de ambiente

---
+++ aula05-variaveis-ambiente-configuracoes-seguraca/app.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+function iniciarAplicacao(){
+    const porta = process.env.PORT || 8080;
+    const apiKey = process.env.API_KEY_PAGAMENTO;
+    const dbUrl = process.env.DATABASE_URL;
+
+    if(!apiKey) {
+        console.error(`[ERRO CRÍTICO]: A chave API_KEY_PAGAMENTO não está definida nas variáveis de ambiente!`);
+        process.exit(1);
+    }
+
+    console.log('===  ||| SERVIÇO DE CONFIGURAÇÃO CARREGADO ||| ===');
+    console.log(`Serviço rodando na porta ${porta}`);
+    console.log(`Banco de dados: ${dbUrl}`);
+    console.log(`APIKey: ${apiKey}`);
+    console.log(`Status da API: Chave de tamanho ${apiKey.length} autenticada`);
+}
+
+iniciarAplicacao();
