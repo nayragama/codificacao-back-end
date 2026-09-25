e8e04048ecfa1592e72c7084a915a4770a06c52a
Nayra Gama <nayrafgama263@gmail.com>
2026-09-24 21:51:43 -0300

    feat: implementar regras de negócio dos convidados

--- aula08-09-metodo-get-post-patch-delete/src/convidados.crontroller.ts
+++ aula08-09-metodo-get-post-patch-delete/src/convidados.crontroller.ts
@@ -1,19 +1,15 @@
-import { Controller, Get, Post, Body } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Delete, Param, HttpCode } from "@nestjs/common";
 import { CriarConvidadoDto } from "./criar-convidados.dto.js";
+import { ConvidadosService } from "./convidados.service.js";
 
 @Controller('convidados')
 export class ConvidadoConstroller {
 
+    constructor(private readonly convidadoService : ConvidadosService){}
+
     @Get()
     listarConvidados(){
-        return [
-           'Rebeca',
-           'Liam',
-           'Cauê',
-           'Nayra',
-           'Jamilly',
-        'Vitória',
-        ];
+        return this.convidadoService.listarConvidados();
     }
     @Post()
     criarConvidado(@Body() criarConvidado: CriarConvidadoDto){
@@ -23,5 +19,19 @@ export class ConvidadoConstroller {
             mensagem: `Convidado(a) ${criarConvidado.nome}, foi adicionado(a) com sucesso `,
             dados: criarConvidado,
         }
+    }
+
+    @Patch(':id')
+    atualizarIdade(@Param('id') id: string,@Body('idade') idade: number){
+        console.log(`[ADMINISTRADOR] Atualizando idade do ID ${id}`);
+        return this.convidadoService.atualizarIdade(+id, idade);
+
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    removerConvidados(@Param('id') id: string){
+        console.log(`[ADMINISTRADOR] Convidado com id ${id} removido com sucesso`);
+        this.convidadoService.removerConvidadoLista(+id);
     }
 }
